f182646b634e1cf11eae7a5493f79971549bcb18
Killmil_Biratch <killmiledc@outlook.com>
2023-08-28 17:05:11 +0800

    Add files via upload

--- js/timeControl.js
+++ js/timeControl.js
@@ -1,11 +1,12 @@
 var date = new Date();
 var h = date.getHours();
-        if(h >= 6 && h <= 18){
-            var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-            headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pDay.css">\r\n    <link rel="stylesheet" href="../../../css/StylePc1080pDay.css">';
-            document.getElementsByTagName('head')[0].innerHTML = headHTML;
-        }else {
-            var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-            headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pNight.css">\r\n    <link rel="stylesheet" href="../../../css/StylePc1080pNight.css">';
-            document.getElementsByTagName('head')[0].innerHTML = headHTML;
-        }
+if(h >= 6 && h <= 18){
+  var headHTML = document.getElementsByTagName('head')[0].innerHTML;
+  headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pDay.css">\r\n    <link rel="stylesheet" href="../../../css/StylePc1080pDay.css">';
+  document.getElementsByTagName('head')[0].innerHTML = headHTML;
+}
+else {
+  var headHTML = document.getElementsByTagName('head')[0].innerHTML;
+  headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pNight.css">\r\n    <link rel="stylesheet" href="../../../css/StylePc1080pNight.css">';
+  document.getElementsByTagName('head')[0].innerHTML = headHTML;
+}
--- js/timeControlIndex.js
+++ js/timeControlIndex.js
@@ -1,11 +1,12 @@
 var date = new Date();
 var h = date.getHours();
-        if(h >= 6 && h <= 18){
-            var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-            headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pDayIndex.css">';
-            document.getElementsByTagName('head')[0].innerHTML = headHTML;
-        }else {
-            var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-            headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pNightIndex.css">';
-            document.getElementsByTagName('head')[0].innerHTML = headHTML;
-        }
+if(h >= 6 && h <= 18){
+  var headHTML = document.getElementsByTagName('head')[0].innerHTML;
+  headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pDayIndex.css">';
+  document.getElementsByTagName('head')[0].innerHTML = headHTML;
+}
+else {
+  var headHTML = document.getElementsByTagName('head')[0].innerHTML;
+  headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pNightIndex.css">';
+  document.getElementsByTagName('head')[0].innerHTML = headHTML;
+}
