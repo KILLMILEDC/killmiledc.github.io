f4a63616b8b852d6dd4afbc418c9a85762601bc5
Killmil_Biratch <killmiledc@outlook.com>
2023-08-28 17:48:00 +0800

    Add files via upload

--- js/timeControlOther.js
+++ js/timeControlOther.js
@@ -2,11 +2,11 @@ var date = new Date();
 var h = date.getHours();
 if(h >= 6 && h <= 18){
   var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-  headHTML    += '    <link rel="stylesheet" href="../../../css/StylePc1080pDay.css">';
+  headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pDay.css">';
   document.getElementsByTagName('head')[0].innerHTML = headHTML;
 }
 else {
   var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-  headHTML    += '    <link rel="stylesheet" href="../../../css/StylePc1080pNight.css">';
+  headHTML    += '    <link rel="stylesheet" href="css/StylePc1080pNight.css">';
   document.getElementsByTagName('head')[0].innerHTML = headHTML;
 }
